3c61f535c6ab486177a7a283a1fbd7a8cdf3f036
won <87519450+won-archive@users.noreply.github.com>
2026-09-11 15:03:37 +0900

    [level 1] Title: 모의고사, Time: 0.90 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,26 +1,47 @@
 def solution(answers):
-    n = len(answers)
-    p1 = [1, 2, 3, 4, 5]
-    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    length = len(answers)   # 문제 수
+    p1 = [1, 2, 3, 4, 5] * (length // 5 + 1)
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5] * (length // 8 + 1)
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (length // 10 + 1)
     
-    score = [0, 0, 0]
-    for i in range(n):
-        if answers[i] == p1[i % len(p1)]:
-            score[0] += 1
-        if answers[i] == p2[i % len(p2)]:
-            score[1] += 1
-        if answers[i] == p3[i % len(p3)]:
-            score[2] += 1
+    result = [0, 0, 0]
+    for i in range(length):
+        if answers[i] == p1[i]:
+            result[0] += 1
+        if answers[i] == p2[i]:
+            result[1] += 1
+        if answers[i] == p3[i]:
+            result[2] += 1
+            
+    maxValue = max(result)
     
-    max_score = max(score)
-    answer = []
-    
-    for i in range(3):
-        if score[i] == max_score:
-            answer.append(i+1)
+    return [i+1 for i, r in enumerate(result) if r == maxValue]
 
-    return answer
+
+
+# def solution(answers):
+#     n = len(answers)
+#     p1 = [1, 2, 3, 4, 5]
+#     p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+#     p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+#     score = [0, 0, 0]
+#     for i in range(n):
+#         if answers[i] == p1[i % len(p1)]:
+#             score[0] += 1
+#         if answers[i] == p2[i % len(p2)]:
+#             score[1] += 1
+#         if answers[i] == p3[i % len(p3)]:
+#             score[2] += 1
+    
+#     max_score = max(score)
+#     answer = []
+    
+#     for i in range(3):
+#         if score[i] == max_score:
+#             answer.append(i+1)
+
+#     return answer
 
 # def solution(answers):
 #     p1 = [1, 2, 3, 4, 5]
