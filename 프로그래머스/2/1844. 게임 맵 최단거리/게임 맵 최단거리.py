df25cea985427abc42b77cd0e744444de3e3f3e9
won <87519450+won-archive@users.noreply.github.com>
2026-09-17 21:10:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.12 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,62 +1,26 @@
+from collections import deque
+
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-    
     dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
+    dy = [1, -1, 0, 0]
     
-    visited = list([0] * m for _ in range(n))
-    answer = -1
-    
-    def bfs(i, j):
-        nonlocal answer
+    visited = [[1 if i == 0 else 0 for i in j] for j in maps]
+    q = deque()
+    q.append((0, 0))
+    visited[0][0] = 1
+    while q:
+        x, y = q.popleft()
         
-        visited[i][j] = 1
-        queue = [(i, j)]
+        if x == n - 1 and y == m - 1:
+            return visited[n - 1][m - 1]
         
-        while queue:
-            x, y = queue.pop(0)
-            
-            if x == n-1 and y == m-1:
-                answer = visited[x][y]
-                break
-            
-            for d in range(4):
-                nx = x + dx[d]
-                ny = y + dy[d]
-                if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 and visited[nx][ny] == 0:
-                    visited[nx][ny] = visited[x][y] + 1
-                    queue.append((nx, ny))
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0:
+                q.append((nx, ny))
+                visited[nx][ny] = visited[x][y] + 1
     
-    bfs(0, 0)
-    
-    return answer
-
-# 시간복잡도: O(n*m)
-# from collections import deque
-
-# def solution(maps):
-#     n = len(maps)
-#     m = len(maps[0])
-
-#     # 상, 하, 좌, 우
-#     dx = [-1, 1, 0, 0]
-#     dy = [0, 0, -1, 1]
-
-#     queue = deque()
-#     queue.append((0, 0))
-
-#     while queue:
-#         x, y = queue.popleft()
-
-#         for i in range(4):
-#             nx = x + dx[i]
-#             ny = y + dy[i]
-
-#             # 맵 안에 있고, 길(1)인 경우만 이동
-#             if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
-#                 maps[nx][ny] = maps[x][y] + 1
-#                 queue.append((nx, ny))
-
-#     # 도착점에 도달하지 못한 경우
-#     if maps[n - 1][m - 1] == 1:
+    return -1
