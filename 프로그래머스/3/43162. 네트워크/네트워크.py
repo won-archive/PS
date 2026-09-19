d03dba1638533c4209e89f733b75f73f6fb2cf91
won <87519450+won-archive@users.noreply.github.com>
2026-09-19 18:57:49 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,36 +1,24 @@
 def solution(n, computers):
-    answer = 0
-    visited = [0 for _ in range(n)]
-    
-    def recur(x):
-        visited[x] = 1
-        
-        for j in range(n):
-            if visited[j] == 0 and computers[x][j]:
-                recur(j)
-
+    adj = [[] for _ in range(n)]
     for i in range(n):
-        if not visited[i]:
-            answer += 1
-            recur(i)
-            
-    return answer
-
-# def solution(n, computers):
-#     visited = [False] * n
-
-#     def dfs(x):
-#         visited[x] = True
-
-#         for y in range(n):
-#             if computers[x][y] == 1 and not visited[y]:
-#                 dfs(y)
-
-#     answer = 0
-
-#     for i in range(n):
-#         if not visited[i]:
-#             dfs(i)
-#             answer += 1
-
-#     return answer
+        for j in range(n):
+            if i == j:
+                continue
+            if computers[i][j]:
+                adj[i].append(j)
+    
+    visited = [0] * n
+    def dfs(num):
+        for a in adj[num]:
+            if not visited[a]:
+                visited[a] = 1
+                dfs(a)
+    
+    ans = 0
+    for k in range(n):
+        if not visited[k]:
+            ans += 1
+            visited[k] = 1
+            dfs(k)
+    
+    return ans
